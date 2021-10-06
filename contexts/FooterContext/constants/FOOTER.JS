63fb4d464b28668389245d7c8889cace6c4b84ac
Anton <baurinanton2013@yandex.ru>
2021-10-06 21:17:41 +0300

    autohelp

---
+++ contexts/FooterContext/constants/FOOTER.JS
@@ -0,0 +1,25 @@
+export const FOOTER = {
+    CONTACTS: [
+        { TEL: '+7 (925) 049 99-62', HREF: 'tel:89250499962' },
+        { TEL: '+7 (919) 082 68-95', HREF: 'tel:89190826895' },
+        { CONTENT: '“Автопомощь.” 2021' },
+    ],
+    MENU: [
+        { TITLE: 'Меню.' },
+        {
+            NAV_LINKS: [
+                { ID: 1, NAME: 'Почему мы?', HREF: '/#whyUs' },
+                { ID: 2, NAME: 'Услуги', HREF: '/#ourServices' },
+                { ID: 3, NAME: 'Цены', HREF: '/prices' },
+                { ID: 4, NAME: 'Работы', HREF: '/#ourWork' },
+                { ID: 5, NAME: 'Шиномонтаж', HREF: '/tireServices' },
+                { ID: 6, NAME: 'Отзывы', HREF: '/reviews' },
+            ]
+        }
+    ],
+    DEVELOPMENT: [
+        {TITLE: 'Сайт запустили.'},
+        {CONTENT: 'Разработка BAURINANTON'},
+        {CONTENT: 'Дизайн BAURINARTYOM'},
+    ]
+}
