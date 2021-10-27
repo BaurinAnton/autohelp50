941ecdcc16f177e57e1206517699f9266c7eabbd
BaurinAnton <baurinanton2013@yandex.ru>
2021-10-27 18:41:57 +0300

    Update Metrika

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
-import Router from "next/router";
-import withYM from "next-ym";
-
 import "../styles/global.scss"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default withYM("86131485", Router)(MyApp);
+export default MyApp;
+
--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,21 @@ class MyDocument extends Document {
     return (
       <Html lang="ru">
         <Head>
+          <script dangerouslySetInnerHTML={{
+            __html: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+         
+            ym(86131485, "init", {
+                 clickmap:true,
+                 trackLinks:true,
+                 accurateTrackBounce:true,
+                 webvisor:true
+            });`}}></script>
+            <noscript dangerouslySetInnerHTML={{
+            __html: `<div><img src="https://mc.yandex.ru/watch/86131485" style="position:absolute; left:-9999px;" alt="" /></div>`}}>
+            </noscript>
           <meta charset="utf-8" />
           <meta name="Author" content="Гуськов Александр" />
           <meta name="Copyright" content="Автопомощь." />
