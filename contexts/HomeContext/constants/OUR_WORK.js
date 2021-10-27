5acf1246d7e8c1ed9d7130a84f85ab84be4c0f03
BaurinAnton <44928991+BaurinAnton@users.noreply.github.com>
2021-10-27 15:00:03 +0300

    Delete contexts directory

--- contexts/HomeContext/constants/OUR_WORK.js
+++
@@ -1,3 +0,0 @@
-export const OUR_WORK = {
-    TITLE: 'Наши работы.'
-}
--- contexts/TireServicesContext/context.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react'
-
-import { WHYOURTIRESERVICE } from './constants'
-
-export const WHYOURTIRESERVICE_CONSTANT = {
-    WHYOURTIRESERVICE
-}
-
-export const WhyOurTireServiceContext = createContext()
